89b8019244fbe8a970173d5a793340ac626bbca7
Ghada <ghadaabdelhady@gmail.com>
2023-12-15 07:51:59 +0200

    task 5

--- resources/views/showPost.blade.php
+++ resources/views/showPost.blade.php
@@ -8,6 +8,7 @@
 <body>
     <h2>{{ $those->title }}</h2>
     <h3>{{ $those->description }}</h3>
+    <h3>by :{{ $those->author }}</h3>
     <h5>{{ $those->updated_at }}</h5>
     <h5>{{ $those->created_at }}</h5>
     <p>{{ $those->published? "Published" : "Not Published" }}</p>
